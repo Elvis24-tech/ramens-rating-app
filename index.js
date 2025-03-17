d267d573f97f473be94ae5c54ff73e018ed349a7
Elvis24-tech <kariuki.elvis@student.moringaschool.com>
2025-03-17 21:14:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,41 +3,52 @@ const ramens = [
         imgUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYEgGOG8fkJ5NAQGu45oHruqnQw3emkIFLBw&s',
         name: 'Shoyu Ramen',
         restaurant: 'Mother Becky Kitchen',
-        rating: 5,
-        comment: 'Yummy and delicious!'
+        ratings: 8,
+        comment: 'Yummy and delicious.'
     },
     {
         imgUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNVvJ6zTa_uY6SaFnFmpwG9FSLckfR9Zwi4g&s',
         name: 'Miso Ramen',
         restaurant: 'Nashlu Base',
-        rating: 4.5,
-        comment: 'Tasty and fingerlicking!'
+        ratings: 6,
+        comment: 'Tasty and fingerlicking.'
     },
     {
         imgUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuiRI_cgax6o_SxuIFLwNnPN4yiWFZP_BIzg&s',
         name: 'Tonkotsu Ramen',
         restaurant: 'Eat and Dip',
-        rating: 3,
-        comment: 'Juicy and Tasty!'
+        ratings: 4,
+        comment: 'Juicy and Tasty.'
     }
 ];
 
 function displayRamens() {
+
     const ramenMenu = document.getElementById('ramen-menu');
+
     ramens.forEach(ramen => {
+
         const ramenImage = document.createElement('img');
+
         ramenImage.src = ramen.imgUrl;
+
         ramenImage.alt = ramen.name;
+
         ramenImage.addEventListener('click', () => handleClick(ramen));
+        
         ramenMenu.appendChild(ramenImage);
     });
 }
 
 function handleClick(ramen) {
     const ramenDetail = document.getElementById('ramen-detail');
+
     document.getElementById('ramen-name').textContent = ramen.name;
+
     document.getElementById('ramen-restaurant').textContent = `Restaurant: ${ramen.restaurant}`;
-    document.getElementById('ramen-rating').textContent = `Rating: ${ramen.rating}`;
+
+    document.getElementById('ramen-ratings').textContent = `Ratings: ${ramen.ratings}`;
+
     document.getElementById('ramen-comment').textContent = `Comment: ${ramen.comment}`;
 }
 
@@ -47,25 +58,35 @@ function addSubmitListener() {
         e.preventDefault();
 
         const imgUrl = document.getElementById('image-url').value;
+
         const name = document.getElementById('ramen-name-input').value;
+
         const restaurant = document.getElementById('ramen-restaurant-input').value;
-        const rating = document.getElementById('ramen-rating-input').value;
+
+        const ratings = document.getElementById('ramen-ratings-input').value;
+
         const comment = document.getElementById('ramen-comment-input').value;
 
         const newRamen = {
             imgUrl,
             name,
             restaurant,
-            rating: rating || 5,
+            ratings: ratings || 10,
             comment
         };
 
         ramens.push(newRamen);
+
         const ramenMenu = document.getElementById('ramen-menu');
+
         const ramenImage = document.createElement('img');
+
         ramenImage.src = newRamen.imgUrl;
+
         ramenImage.alt = newRamen.name;
+
         ramenImage.addEventListener('click', () => handleClick(newRamen));
+
         ramenMenu.appendChild(ramenImage);
         
         form.reset();
